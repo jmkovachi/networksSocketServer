00d332648810f42506c338a637eef1ed3b5d7e60
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-11-05 12:44:22 -0500

    Change client output formatting

--- client.py
+++ client.py
@@ -7,10 +7,14 @@ PORT = sys.argv[2]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((HOST, int(PORT)))
-	message = "GET /" + sys.argv[3] + " HTTP/1.0\nHost: " + HOST + "\r\n"
-	s.sendall(message.encode())
+	request = "GET /" + sys.argv[3] + " HTTP/1.0\nHost: " + HOST + "\r\n"
+	print("Connecting to server with request: \n", request)
+	s.sendall(request.encode())
 	data = s.recv(4096)
+	print("Response: \n")
 	print(data.decode())
 	data = s.recv(4096)
 	print(data.decode())
 	s.close()
+
+	
